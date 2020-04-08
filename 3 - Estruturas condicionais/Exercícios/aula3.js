25ec92fc18bd1968012f8bc7f08266d46978ea6e
millena gomes <millenagomes29@gmail.com>
2020-04-08 17:12:35 -0300

    Correção dos Exercicios 08/04

---
+++ 3 - Estruturas condicionais/Exercícios/aula3.js
@@ -0,0 +1,171 @@
+//**1.** Faça um algoritmo que receba uma letra e determine se é uma vogal ou consoante 
+//(Faça duas versões deste código, uma utilizando if-else e outra utilizando switch-case);
+
+/*const letra = prompt('Digite uma letra do alfabeto');
+
+if (letra === 'a'
+ || letra === 'e'
+ || letra === 'i'
+ || letra === 'o'
+ || letra === 'u') {
+    alert(`A letra ${letra} é vogal!`);
+
+ } else {
+    alert(`A letra ${letra} é uma consoante`)
+ } /*
+
+ 
+
+ /* Fazendo com switch:
+ switch (letra) {
+    case 'a': // Colocamos os cases seguidos um do outro para economizar
+    case 'e': // código, já que todos eles terão a mesma mensagem
+    case 'i':
+    case 'o':
+    case 'u':
+        alert(`A letra ${letra} é uma vogal`);
+        break; // se n houver break 
+        default:
+            alert(`A letra ${letra} é uma consoante´);*/
+
+
+ //**2.** Escreva um código que receba um número (1-7) e devolva o dia da semana correspondente;
+ //const diaSemana = parseInt(prompt('Digite o dia da semana [1-7]'));
+
+/*switch (diaSemana) {
+    case 1:
+        alert(`Domingo`);
+        break;
+    case 2:
+        alert(`Segunda-Feira`);
+        break;
+    case 3:
+        alert(`Terça-Feira`);
+        break;
+    case 4:
+        alert(`Quarta-Feira`);
+        break;
+    case 5:
+        alert(`Quinta-Feira`);
+        break;
+    case 6:
+        alert(`Sexta-Feira`);
+        break        
+    case 7:
+        alert(`Sábado`);
+        break;
+    default:
+        alert(`Dia de São Nunca`);
+} */
+
+//**3.** Escreva um código que receba um número (1-12), e diga qual mês e quantos dias ele tem;
+
+/* let numerosMes = parseInt(prompt(`Digite um número de (1 a 12)`));
+numerosMes = parseInt(numerosMes);
+
+if (numeroMes === 1) {
+    alert(`Janeiro é o mês ${numerosMes} e possui 31 dias`);
+} else if (numeroMes === 2) {
+    alert(`Fevereiro é o mês ${numerosMes} e possui 28 ou 29 dias`);
+} else if (numeroMes === 3) {
+    alert(`Março é o mês ${numerosMes} e possui 31 dias`);
+} else if (numeroMes === 4) {
+    alert(`Abril é o mês ${numerosMes} e possui 30 dias`);
+} else if (numeroMes === 5) {
+    alert(`Maio é o mês ${numerosMes} e possui 31 dias`);
+} else if (numeroMes === 6) {
+    alert(`Junho é o mês ${numerosMes} e possui 30 dias`);
+} else if (numeroMes === 7) {
+    alert(`Julho é o mês ${numerosMes} e possui 31 dias`);
+}
+ else if (numeroMes === 8) {
+    alert(`Agosto é o mês ${numerosMes} e possui 31 dias`);
+} else if (numeroMes === 9) {
+    alert(`Setembro é o mês ${numerosMes} e possui 30 dias`);
+} else if (numeroMes === 10) {
+    alert(`Outubro é o mês ${numerosMes} e possui 31 dias`);
+} else if (numeroMes === 11) {
+    alert(`Novembro é o mês ${numerosMes} e possui 30 dias`);
+} else if (numeroMes === 12) {
+    alert(`Dezembro é o mês ${numerosMes} e possui 31 dias`);
+} else {
+    alert('Não é um mês conhecido');
+}
+
+
+ **4.** Escreva um código que receba dois número e determine qual o maior entre eles;
+
+const num1 = prompt('Digite o primeiro número:');
+const num2 = prompt('Digite o segundo número:');
+
+if (num1 > num2) {
+    alert(`${num1} é maior que ${num2}`);
+} else if (num1 === num2) {
+    alert(`${num1} é igual a ${num2}`);
+} else {
+    alert(`${num2} é maior que ${num1}`);
+} */
+
+// **5.** Escreva um código que receba um número e determine se ele é par ou ímpar, e se é negativo ou positivo;
+
+/* const numeroP = parseInt(prompt('Digite um número'));
+
+if (numeroP%2 === 0 && numero>0){
+    alert(`${numeroP} é par, e ele é positivo`) 
+} else if(numeroP%2 === 0 && numeroP<0) {
+    alert(`${numeroP} é par, e ele é negativo`)
+} else if(numeroP%2 !== 0 && numeroP>0) {
+    alert(`${numeroP} é impar, e ele é positivo`)
+} else if(numeroP%2 !== 0 && numeroP<0) {
+    alert(`${numeroP} é par, e ele é negativo`)
+} else {
+    alert(`${numeroP} é um número neutro`)
+} */
+
+//**6.** Escreva um código que receba três ângulos e determine se eles formam um triângulo válido.
+//Em um triângulo válido, a soma dos três ângulos é igual a 180º.
+
+//const angulo1 = parseInt(prompt('Digite o ângulo 1'));
+//const angulo2 = parseInt(prompt('Digite o ângulo 2'));
+//const angulo3 = parseInt(prompt('Digite o ângulo 3'));
+//const somaAngulos = angulo1 + angulo2 + angulo3;
+
+//if (somaAngulos === 180) {
+//    alert('o triângulo é válido');
+//} else{ 
+//    alert('O triângulo não é valido');
+//}
+
+//**7.**. Elaborar um algoritmo que lê 2 valores a e b e informa se são múltiplos ou não.
+
+/*const valorA = parseInt(prompt('Digite um valor'));
+const valorB = parseInt(prompt('Agora digite um outro valor!'));
+const resto = valorA%valorB;
+
+if (resto === 0) {
+    alert('Eles são múltiplos!');
+} else {
+    alert('Eles não são múltiplos');
+} */
+
+
+/* **8.** Crie um código que retorne a quantidade de pontos que seu time fez de acordo com o resultado do jogo. 
+(Se o seu time venceu, ele recebe 3 pontos. Se o resultado foi empate, ele recebe 1 ponto. Se ele perdeu, não 
+//recebe nada.) */
+
+//const resultadoJogo
+//const Time
+
+
+
+/* **9.** Elabore um programa que dada a idade de um nadador classifica-o em uma das seguintes categorias:
+* Infantil A = 5 - 7 anos
+* Infantil B = 8-10 anos
+* Juvenil A = 11-13 anos
+* Juvenil B = 14-17 anos
+* Adulto = maiores de 18 anos */
+
+//if (vari >=5 && var <=7) {
+ 
+//}
+
---
+++ 4 - Estruturas de repetição/Exercícios/aula4.js
@@ -0,0 +1,11 @@
+//**1.** Faça um algoritmo que imprima números inteiros de n a 1;
+
+const numeroInteiro = parseInt(prompt('Digite um valor')); 
+
+for (let numeroInteiro = ; i<= 100; i++) {
+    console.log (i)
+}
+
+//**2.** Faça um algoritmo que imprima apenas os números ímpares de 1 a 100; (do-while)
+
+//**3.** Dados dois números calcule o MDC deles.
